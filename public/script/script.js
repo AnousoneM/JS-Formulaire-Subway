c953bdcf46f28c165b069ba2b65f7077da080c3d
Anou <anousone.mounivongs@gmail.com>
2022-05-11 10:53:16 +0200

    js verification des inputs

--- public/script/script.js
+++ public/script/script.js
@@ -1,9 +1,52 @@
-function cleanError(id){
-    console.log('er')
-    let elementId = id+'Error';
-    console.log(elementId)
-    let element = document.getElementById(elementId);
-    console.log(element)
-    element.innerHTML = ''
-}
 
+// Fonction permettant de vérifier mes données entrées
+function verifyInputs() {
+    // déclaration des éléments
+    let lastname = document.getElementById('lastname');
+    let firstname = document.getElementById('firstname');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let confirmPassword = document.getElementById('confirmPassword');
+
+
+    // verification du nom
+    // attention il s'agit d'un input, pour récupérer valeur on utilise .value
+    if (lastname.value == '') {
+        let lastnameError = document.getElementById('lastnameError');
+        lastnameError.innerHTML = 'Veuillez saisir un nom';
+
+    }
+
+    // verification du prénom
+    if (firstname.value == '') {
+        let firstnameError = document.getElementById('firstnameError');
+        firstnameError.innerHTML = 'Veuillez saisir un prénom';
+    }
+
+    // verification du email
+    if (email.value == '') {
+        let emailError = document.getElementById('emailError');
+        emailError.innerHTML = 'Veuillez saisir un email';
+    }
+
+    // verification du password et du confirmPassword
+    let passwordError = document.getElementById('passwordError');
+    if (password.value == '') {
+        passwordError.innerHTML = 'Veuillez saisir un mot de passe';
+    } else if (confirmPassword.value == '') {
+        passwordError.innerHTML = 'Veuillez confirmer votre mot de passe';
+    } else if (password.value != confirmPassword.value){
+        passwordError.innerHTML = 'les mots de passes sont différents';
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
